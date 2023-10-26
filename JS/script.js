08bb90e412fc18ab115d1202563073ad076f7ed1
Ignacio Alonso <nachoo.alonso@gmail.com>
2023-10-25 21:57:53 -0300

    js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,34 @@
 /* FORMAS DE CREAR VARIABLES
+---- var : Para variables globales (es mala practica y ya no se usa)
+---- let : Para variables locales y no constantes.
+---- const : Para variables locales y constantes.
+*/
 
----- var: Para variables 
+let numero1 = 0;
+let numero2 = 2;
+
+function sumar(){
+   return numero1 + numero2;
+}
+
+const resulado = sumar();
+console.log(resulado);
+
+// Callback: Es una funcion que se envia por parametro a una funcion/metodo
+
+let dias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+
+/*
+dias.forEach((item)=>{
+    console.log(item);
+});
+*/
+
+const callback = (item) =>{console.log(item)}   // FUNCION FLECHA --- CUANDO ES MAS DE UNA INSTRUCCION HAY QUE ABRIR LLAVES {}
+dias.forEach(callback);
+
+/*
+funtion callback(elemento){
+    console.log(elemento);
+}
+*/
